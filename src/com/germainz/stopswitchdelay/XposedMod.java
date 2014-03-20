8ec84ebf3fa89df4524302712f441b5f8e4c8028
GermainZ <germanosz@gmail.com>
2014-03-20 23:53:47 +0200

    Initial commit

---
+++ src/com/germainz/stopswitchdelay/XposedMod.java
@@ -0,0 +1,22 @@
+package com.germainz.stopswitchdelay;
+
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodReplacement;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class XposedMod implements IXposedHookZygoteInit {
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+
+        findAndHookMethod("com.android.server.am.ActivityManagerService", null, "stopAppSwitches", new XC_MethodReplacement() {
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                return null;
+            }
+        });
+
+    }
+
+}
