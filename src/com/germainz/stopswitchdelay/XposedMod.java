310d4e7f1f30dcaa1cfebf4da21d1d35e30bfb2c
GermainZ <germanosz@gmail.com>
2015-10-25 11:35:33 +0200

    Update for Xposed Lollipop

--- src/com/germainz/stopswitchdelay/XposedMod.java
+++ src/com/germainz/stopswitchdelay/XposedMod.java
@@ -1,16 +1,19 @@
 package com.germainz.stopswitchdelay;
 
-import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
-public class XposedMod implements IXposedHookZygoteInit {
+public class XposedMod implements IXposedHookLoadPackage {
 
     @Override
-    public void initZygote(StartupParam startupParam) throws Throwable {
-        findAndHookMethod("com.android.server.am.ActivityManagerService", null, "stopAppSwitches",
-                XC_MethodReplacement.returnConstant(null));
-    }
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("android"))
+            return;
 
+        findAndHookMethod("com.android.server.am.ActivityManagerService", lpparam.classLoader,
+                "stopAppSwitches", XC_MethodReplacement.returnConstant(null));
+    }
 }
